c876a35f7e9b1e1010bee792feae4250c5cfdc98
lucasloepke <wavyknight72@gmail.com>
2024-07-18 11:59:56 -0400

    update

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ def start_progress():
     if complete:
         text.pack(side='bottom', pady=10)
         m.update_idletasks()
-        m.after(2000, text.config(text='Program Ending...'))
+        m.after(1200, text.config(text='Program Ending...'))
         m.title('Goodbye!')
         button.config(text='Goodbye!')
         m.update_idletasks()
@@ -37,28 +37,29 @@ def selsta():
         stabut.config(bg='spring green', text='Statements Selected ✅')
 
 m = tk.Tk(screenName=None,  baseName=None,  className='Tk',  useTk=1)
-m.title('Internal Tool')
+m.title('SAC Expensive Statements Tool')
 complete = False
 
 # Select files button widgets
-actbut = tk.Button(m, text="Select Actions", command=selact)
-stabut = tk.Button(m, text="Select Statements", command=selsta, activebackground='white')
+actions = ''; statements = ''
+actbut = tk.Button(m, text="Select Actions", command=selact, pady=5, padx=5)
+stabut = tk.Button(m, text="Select Statements", command=selsta, activebackground='white', padx=5, pady=5)
 
 # Create a progressbar widget
-progress = ttk.Progressbar(m, style='success.Striped.Horizontal.TProgressbar', length=300, mode="determinate")
+progress = ttk.Progressbar(m, style='', length=300, mode="determinate")
 
 # Button to start progress
 start_button = tk.Button(m, text="Create Summary", command=start_progress, bg='turquoise', activebackground='dark turquoise')
 
-label = tk.Label(m, text='SAC-Tools')
+label = tk.Label(m, text='Select Files and Create Summary!')
 text = tk.Label(m, text='Summary Created!')
 button = tk.Button(m, text='End Process', width=25, command=m.destroy, background='lightgray')
 
 m.minsize(400, 300)
 m.maxsize(400, 300)
 label.pack(anchor='n')
-actbut.place(x=50,y=25)
-stabut.place(x=50,y=60)
+stabut.place(x=50,y=25)
+actbut.place(x=50,y=70)
 start_button.place(x=150,y=150)
 progress.place(x=50, y=190)
 button.pack(side='bottom', pady=10)
